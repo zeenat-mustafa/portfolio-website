59fcf2714dd096c113bbcef3f8f92cb63db5dd3c
Zeenat Mustafa <chzeenatmustafa2310@gmail.com>
2025-04-26 23:02:47 +0500

    Add Services Section
    - Add services sectio and its styling
    - Add modals

--- scripts/main.js
+++ scripts/main.js
@@ -3,3 +3,26 @@ window.addEventListener("scroll", function(){
     const header = this.document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
 })
+
+// Services section - Modal
+const serviceModals = document.querySelectorAll(".service-modal");
+const learnmoreBtns = document.querySelectorAll(".learn-more-btn");
+const modalCloseBtns = document.querySelectorAll(".modal-close-btn");
+
+var modal = function(modalClick){
+    serviceModals[modalClick].classList.add("active");
+}
+
+learnmoreBtns.forEach((learnmoreBtns, i) => {
+    learnmoreBtns.addEventListener("click", () => {
+        modal(i);
+    });
+});
+
+modalCloseBtns.forEach((modalCloseBtn) => {
+    modalCloseBtn.addEventListener("click", () => {
+        serviceModals.forEach((modalView) => {
+            modalView.classList.remove("active");
+        });
+    });
+});
