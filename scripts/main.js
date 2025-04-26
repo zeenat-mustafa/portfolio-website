ae03efff2243f7e708859bc9f087a34d0fcaf311
Zeenat Mustafa <chzeenatmustafa2310@gmail.com>
2025-04-26 20:57:26 +0500

    Add Navigation bar effects on scroll
    - Add scroll bar colors
    - Add Navigation bar effects

---
+++ scripts/main.js
@@ -0,0 +1,5 @@
+// Navigation bar effects on scroll
+window.addEventListener("scroll", function(){
+    const header = this.document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+})
