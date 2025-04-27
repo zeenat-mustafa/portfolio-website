93dcd0af16977d3c711fa0434c8067edc61fe335
Zeenat Mustafa <chzeenatmustafa2310@gmail.com>
2025-04-27 10:58:19 +0500

    Add Portfolio Section
    - Add HTML structure for Portfolio
    - Add styling for portfolio section
    - Add modal for the portfolio cards

--- scripts/main.js
+++ scripts/main.js
@@ -26,3 +26,26 @@ modalCloseBtns.forEach((modalCloseBtn) => {
         });
     });
 });
+
+// Portfolio section - Modal
+const portfolioModals = document.querySelectorAll(".portfolio-model");
+const imgCards = document.querySelectorAll(".img-card");
+const portfolioCloseBtns = document.querySelectorAll(".portfolio-close-btn");
+
+var portfolioModal = function(modalClick){
+    portfolioModals[modalClick].classList.add("active");
+}
+
+imgCards.forEach((imgCard, i) => {
+    imgCard.addEventListener("click", () => {
+        portfolioModal(i);
+    });
+});
+
+portfolioCloseBtns.forEach((portfolioCloseBtn) => {
+    portfolioCloseBtn.addEventListener("click", () => {
+        portfolioModals.forEach((portfolioModalView) => {
+            portfolioModalView.classList.remove("active");
+        });
+    });
+});
