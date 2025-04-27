595799747fc1e8906d0d3dbdccf7150472804349
Zeenat Mustafa <chzeenatmustafa2310@gmail.com>
2025-04-27 17:21:13 +0500

    Add theme
    - Add Dark theme to the document

--- scripts/main.js
+++ scripts/main.js
@@ -65,6 +65,28 @@ var swiper = new Swiper(".client-swiper", {
     },
   });
 
+// Light/Dark theme
+const themeBtn = document.querySelector(".theme-btn");
+
+themeBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark-theme");
+    themeBtn.classList.toggle("sun");
+
+    localStorage.setItem("saved-theme", getCurrentTheme());
+    localStorage.setItem("saved-icon", getCurrentIcon());
+});
+
+const getCurrentTheme = () => document.body.classList.contains("dark-theme") ? "dark" : "light";
+const getCurrentIcon = () => themeBtn.classList.contains("sun") ? "sun" : "moon";
+
+const savedTheme = localStorage.getItem("saved-theme");
+const savedIcon = localStorage.getItem("saved-icon");
+
+if(savedTheme){
+    document.body.classList[savedTheme === "dark" ? "add" : "remove"]("dark-theme");
+    themeBtn.classList[savedIcon === "sun" ? "add" : "remove"]("sun");
+}
+
 // Scroll to Top Button
 const scrollTopBtn = document.querySelector(".scrollToTop-btn");
 
