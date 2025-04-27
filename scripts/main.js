b13094b02acdbcb0929002de13ebda83b7f959d0
Zeenat Mustafa <chzeenatmustafa2310@gmail.com>
2025-04-27 13:58:07 +0500

    Add Client Section
    - Add html structure for client Section
    - Add styling for client Section
    - Use swiper Js for slides

--- scripts/main.js
+++ scripts/main.js
@@ -49,3 +49,18 @@ portfolioCloseBtns.forEach((portfolioCloseBtn) => {
         });
     });
 });
+
+// Our Client - Swiper
+var swiper = new Swiper(".client-swiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
