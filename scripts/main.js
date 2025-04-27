811839dc6a3bc8aa6baa77c6611ae4832a8cbbdf
Zeenat Mustafa <chzeenatmustafa2310@gmail.com>
2025-04-27 15:26:33 +0500

    Add Button
    - Add button which scrolls to the top
    - Add Styling and script

--- scripts/main.js
+++ scripts/main.js
@@ -64,3 +64,15 @@ var swiper = new Swiper(".client-swiper", {
       prevEl: ".swiper-button-prev",
     },
   });
+
+// Scroll to Top Button
+const scrollTopBtn = document.querySelector(".scrollToTop-btn");
+
+window.addEventListener("scroll", function(){
+    scrollTopBtn.classList.toggle("active", window.scrollY > 500);
+});
+
+scrollTopBtn.addEventListener("click", () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+});
